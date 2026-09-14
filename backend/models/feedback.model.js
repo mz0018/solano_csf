7088fa999be2f1d4c5ffda3788a905f22233919e
mz0018 <martinezhanzmenzi@gmail.com>
2026-09-14 08:30:52 +0800

    fixed duplicate removed index on officeCode 201

--- backend/models/feedback.model.js
+++ backend/models/feedback.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const feedbackSchema = new mongoose.Schema({
-    officeCode: { type: String, required: true, unique: true, index: true },
+    officeCode: { type: String, required: true, index: true },
     queueNumber: { type: String, required: true, index: true },
 
     service: { type: String, required: true, index: true },
